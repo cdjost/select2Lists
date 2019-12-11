4f21a51d595738ef447d69428b5d9379de919460
Christoph Jost <christoph.d.jost@googlemail.com>
2019-12-11 17:40:01 +0100

    Changes

--- src/main/java/de/select2/HomePage.java
+++ src/main/java/de/select2/HomePage.java
@@ -1,5 +1,7 @@
 package de.select2;
 
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LambdaModel;
 import org.apache.wicket.model.Model;
@@ -14,6 +16,8 @@ import java.util.List;
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
+	Select2Choice<List<EState>> stateSelect;
+
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
 
@@ -25,8 +29,19 @@ public class HomePage extends WebPage {
 		List<EState> selectedStates = new LinkedList<>();
 		selectedStates = EState.LIST_1;
 		IModel<List<EState>> selectedStatesModel = Model.ofList(selectedStates);
-		Select2Choice<List<EState>> stateSelect = new Select2Choice<>("selectState", selectedStatesModel, new StateProvider(states));
-		queue(stateSelect);
+		stateSelect = new Select2Choice<>("selectState", selectedStatesModel, new StateProvider(states));
+
+		Form<Void> form = new Form<>("form");
+
+		form.add(stateSelect);
+		form.add(new Button("submit", Model.of("Submit")));
+		add(form);
+	}
+
+	@Override
+	protected void onConfigure(){
+		super.onConfigure();
+		System.out.println(stateSelect.getModelObject());
 	}
 
 
